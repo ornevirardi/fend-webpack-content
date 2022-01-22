31ca255dd067b96e695ea7c14a18a4260f3ef765
Orne Virardi <ornellavirardi@gmail.com>
2022-01-22 15:50:00 -0800

    ewer

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,9 +2,10 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
+
 module.exports = {
     entry: './src/client/index.js',
-    devtool: 'source-map',
+    mode: 'production',
     module: {
         rules: [
             {
