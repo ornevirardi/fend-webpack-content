d63bfc54051ebf8cd830f2b2049c834bee5fb96b
Orne Virardi <ornellavirardi@gmail.com>
2022-01-22 16:06:44 -0800

    kj

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 
 module.exports = {
@@ -31,5 +32,6 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
     }),
+    new BundleAnalyzerPlugin()
     ]
 }
